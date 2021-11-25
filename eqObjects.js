55896b52b18e5d693a3a9be0e164402e0a322696
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-11-25 04:49:11 +0000

    ading eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,104 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+}; 
+
+const eqArrays = function(arr1, arr2) {
+  let bool = true;
+  if (arr1.length !== arr2.length) {
+    bool = false; 
+    return bool;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      bool = false;
+      return bool;    // Exits function the moment bool is false 
+    }
+  }
+  return bool;  //bool is true by default
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+
+
+const eqObjects = function(object1, object2) {
+  let arr1 = Object.keys(object1);
+  let arr2 = Object.keys(object2);
+  let bool = true;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (const key of arr1) {
+      if (Array.isArray(object1[key])) {
+          if (!eqArrays(object1[key], object2[key])) {
+            return false;
+          }
+        } else if (object1[key] !== object2[key]) {
+           return false;    
+        } 
+      }
+   return true;   
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+//eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+//console.log(eqObjects(cd, cd2)); // => false
+
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
+
+
+
+
+
+
+/* for primitive data types
+const eqObjects = function(object1, object2) {
+  let arr1 = Object.keys(object1);
+  let arr2 = Object.keys(object2);
+  let bool = false;
+  if (arr1.length === arr2.length) {
+    for (const key1 of arr1) {
+      for (const key2 of arr2) {
+         if (key1 === key2) {
+              object1[key1] = object2[key2];
+              bool = true;
+          }
+      }
+    }
+  }
+  return bool;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual(eqObjects(ab, abc), false); // => false
+assertEqual(eqObjects(ab, ba), true);   // => true  */
+
+//const cd = { c: "1", d: ["2", 3] };
+//const dc = { d: ["2", 3], c: "1" };
+//eqObjects(cd, dc); // => true
+
+//const cd2 = { c: "1", d: ["2", 3, 4] };
+//eqObjects(cd, cd2); // => false
+
+//assertEqual(eqObjects(cd, dc), true);
+//assertEqual(eqObjects(cd, cd2), false);
+
+
+//console.log(eqArrays(cd["d"], dc["d"]));
+
+
+
+
