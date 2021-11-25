2e82bdccab6e170bcb9e612d9492493d428f6bde
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-11-25 03:16:35 +0000

    adding findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+const findKeyByValue = function(obj, val) {
+
+  for (const key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
