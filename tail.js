7a1c65ad2c4d66aac536dfcdb72abfec373154ad
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-11-29 21:03:35 +0000

    Refactored & made DRY

--- tail.js
+++ tail.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(array) {
   let temp = [];
@@ -19,19 +11,4 @@ const tail = function(array) {
   return temp;
 };
 
-const result = (tail(["Yo Yo", "Lighthouse", "Labs"]));
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-/*const result = (tail(["Yo Yo"]));
-assertEqual(result.length, 0); // ensure we get back four elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(result[2], "Chandler");
-assertEqual(result[3], "Ross");
-
-const result = (tail([]));
-assertEqual(result.length, 0); // ensure we get back 0 elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"*/
+module.exports = tail;
