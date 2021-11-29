d7b9007dea526b2607a0a9bf69ecb6fbf0981291
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-11-29 21:52:16 +0000

    Refactored & made DRY

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let bool = true;
-  if (arr1.length !== arr2.length) {
-    bool = false; 
-    return bool;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      bool = false;
-      return bool;    // Exits function the moment bool is false 
-    }
-  }
-  return bool;  //bool is true by default
-};
+const eqArrays = require('./eqArrays');
 
 
 // FUNCTION IMPLEMENTATION
@@ -25,9 +12,6 @@ const assertArraysEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertArraysEqual([1, 1, 3], [1, 2, 3]); // false
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
-assertArraysEqual([1, 4, 3], [1, 2, 3]); // false
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // false
+module.exports = assertArraysEqual;
+
 
